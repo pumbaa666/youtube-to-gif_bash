2bc101f446fc5daac4e280f683b46c3a87408f1d
loic <loic@pryv.com>
2020-03-06 17:38:45 +0100

    add basic readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example
+
+`./youtube-to-gif.sh https://www.youtube.com/watch?v=gn--7WFOlx8 00:00:30 00:00:35`
--- youtube-to-gif.sh
+++ youtube-to-gif.sh
@@ -1,8 +1,5 @@
 # Extract a portion of a youtube video, make it a gif and upload it to giphy#
 #
-# Example
-# ./youtube-to-gif.sh https://www.youtube.com/watch?v=gn--7WFOlx8 00:00:30 00:00:35
-#
 # TODO param list, help, etc
 
 # parameters with default values
